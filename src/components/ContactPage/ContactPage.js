556b38c305539cb8394351f8a9aefc1f36612f45
seokungarna <64422363+seokungarna@users.noreply.github.com>
2025-04-04 11:41:12 +0200

    Update ContactPage.js

--- src/components/ContactPage/ContactPage.js
+++ src/components/ContactPage/ContactPage.js
@@ -24,7 +24,7 @@ const ContactPage = () => (
                 {/* <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path> */}
                 {/* <circle cx="12" cy="10" r="3"></circle> */}
               {/* </svg>Balcon de Benavista, El Paraiso, 29688 Estepona */}
-            </a>
+            {/* </a> */}
                       
             <a className="Contact--Details--Item" href="tel+34 635 43 43 06">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
